730b133d1486e28160c8107237cff135c1de3761
hchaejeong <hchaejeong@kaist.ac.kr>
2024-01-13 20:56:37 +0900

    finished xml files

--- app/src/main/java/com/example/closetfrontend/AddPhotoActivity.java
+++ app/src/main/java/com/example/closetfrontend/AddPhotoActivity.java
@@ -1,14 +1,26 @@
 package com.example.closetfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AddPhotoActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+
+    private ArrayList<String> imagePathArrayList;
+    private RecyclerView recyclerView;
+    private ImageAdapter imageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_photo);
+
+        selectedImage = findViewById(R.id.RVImages)
     }
+
+
 }
---
+++ app/src/main/java/com/example/closetfrontend/GalleryAdapter.java
@@ -0,0 +1,64 @@
+package com.example.closetfrontend;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ImagesViewHolder> {
+
+    private Context context;
+    private ArrayList<String> imagePathArrayList;
+
+    public GalleryAdapter(Context context, ArrayList<String> imagePathArrayList) {
+        this.context = context;
+        this.imagePathArrayList = imagePathArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ImagesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.image_list, parent, false);
+        return new GalleryAdapter.ImagesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImagesViewHolder holder, int position) {
+        File imageFile = new File(imagePathArrayList.get(holder.getAdapterPosition()));
+
+        if (imageFile.exists()) {
+            Picasso.get().load(imageFile).placeholder(R.drawable.ic_launcher_background).into(holder.imageIV);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(context, )
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return imagePathArrayList.size();
+    }
+
+    public class ImagesViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageIV;
+
+        public ImagesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageIV = itemView.findViewById(R.id.idIVImage);
+        }
+    }
+}
